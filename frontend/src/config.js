87c4803336d81e52e21ad2946acebccbac1b2610
martu <camposm@aia.es>
2021-05-07 01:02:34 +0200

    Fixed the place to define publicPath

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,3 @@
 module.exports = {
     apiPath: process.env.NODE_ENV === 'production' ? 'https://live-tda.herokuapp.com/api/' : 'http://localhost:8000/api/',
-    publicPath: process.env.NODE_ENV === 'production' ? '/live-tda/' : '/',
 }
