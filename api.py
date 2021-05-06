179c6c0759c43d868d99a98ab41c1d5ca238d86d
martu <camposm@aia.es>
2021-05-07 01:11:27 +0200

    Added GitHub pages as allowed origin to API

--- api.py
+++ api.py
@@ -49,7 +49,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost", "http://localhost:8080"],
+    allow_origins=["http://localhost", "http://localhost:8080", "https://doctorfields.github.io/live-tda/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
