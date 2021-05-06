63425b2b04345847af0616bd397e9e6f71a18225
martu <camposm@aia.es>
2021-05-07 00:51:13 +0200

    Added public path to Vue app config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,4 @@
 module.exports = {
     apiPath: process.env.NODE_ENV === 'production' ? 'https://live-tda.herokuapp.com/api/' : 'http://localhost:8000/api/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/live-tda/' : '/',
 }
