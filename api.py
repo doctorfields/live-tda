7e761b1c74be6c2788ebd7d19653dd112e0680c3
martu <camposm@aia.es>
2021-05-07 01:19:49 +0200

    Allow any origin

--- api.py
+++ api.py
@@ -49,7 +49,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost", "http://localhost:8080", "https://doctorfields.github.io/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
